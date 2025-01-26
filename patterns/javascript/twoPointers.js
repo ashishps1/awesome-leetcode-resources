aa1fbcc9d84aa1d772bbcbc19fc5ceeb10bb02a9
Ashish Pratap Singh <ashk43712@gmail.com>
2025-01-26 19:22:53 +0530

    add two pointers pattern

---
+++ patterns/javascript/twoPointers.js
@@ -0,0 +1,68 @@
+class TwoPointers {
+    /**
+     * Move Zeroes using Two Pointers
+     * @param {number[]} nums - Input array
+     */
+    moveZeroesTwoPointers(nums) {
+        let left = 0; // Pointer for placing non-zero elements
+
+        // Iterate with right pointer
+        for (let right = 0; right < nums.length; right++) {
+            if (nums[right] !== 0) {
+                // Swap elements if right pointer finds a non-zero
+                [nums[left], nums[right]] = [nums[right], nums[left]];
+                left++; // Move left pointer forward
+            }
+        }
+    }
+
+    /**
+     * Brute Force approach for Container with Most Water
+     * @param {number[]} height - Array of heights
+     * @return {number} - Maximum water that can be contained
+     */
+    maxAreaBruteForce(height) {
+        let maxArea = 0;
+        let n = height.length;
+
+        // Check all pairs (i, j)
+        for (let i = 0; i < n; i++) {
+            for (let j = i + 1; j < n; j++) {
+                // Compute the minimum height and width
+                let minHeight = Math.min(height[i], height[j]);
+                let width = j - i;
+                let area = minHeight * width; // Compute water contained
+
+                maxArea = Math.max(maxArea, area); // Update max water
+            }
+        }
+        return maxArea;
+    }
+
+    /**
+     * Two Pointers approach for Container with Most Water
+     * @param {number[]} height - Array of heights
+     * @return {number} - Maximum water that can be contained
+     */
+    maxAreaTwoPointers(height) {
+        let left = 0, right = height.length - 1;
+        let maxArea = 0;
+
+        // Move pointers toward each other
+        while (left < right) {
+            let width = right - left; // Distance between lines
+            let minHeight = Math.min(height[left], height[right]); // Compute height
+            let area = minHeight * width; // Compute water contained
+
+            maxArea = Math.max(maxArea, area); // Update max water
+
+            // Move the pointer pointing to the shorter height
+            if (height[left] < height[right]) {
+                left++; // Move left pointer forward
+            } else {
+                right--; // Move right pointer backward
+            }
+        }
+        return maxArea;
+    }
+}
